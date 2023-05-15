5e98ae6b8fdacf7304fc5d235957c305bf4b4a17
Reekomer <jeremy.degail@gmail.com>
2023-05-15 16:59:32 +0200

    Remove a hardcoded Django secret key

--- simple_multivendor_site/settings.py
+++ simple_multivendor_site/settings.py
@@ -11,10 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(w0xd82mx=l#v^w)83zhzwc&))6#+9&#e(87y7a8mo=)6!=vz8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
